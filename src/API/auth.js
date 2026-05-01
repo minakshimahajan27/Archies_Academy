67586b24ba900ac835e696de89efdd9be1282060
minakshimahajan27 <minakshi27mahajan2211@>
2026-05-01 11:25:28 +0530

    connect backend

--- src/API/auth.js
+++ src/API/auth.js
@@ -1,16 +1,16 @@
 
 
-export const API_BASE_URL = "http://localhost:5000"; 
+export const API_BASE_URL = "[https://archies-academy.onrender.com/api](https://archies-academy.onrender.com/api)"; 
 
-export const ADMIN_LOGIN = `${API_BASE_URL}/api/admin/login`;
+export const ADMIN_LOGIN = `${API_BASE_URL}/admin/login`;
 
-export const ADD_PACKAGE = `${API_BASE_URL}/api/admin/packages/add`;
-export const GET_PACKAGE = `${API_BASE_URL}/api/admin/packages/all`;
-export const UPDATE_PACKAGE = `${API_BASE_URL}/api/admin/packages/update/:id`; // Backend route: /api/packages/update/:id
-export const DELETE_PACKAGE = `${API_BASE_URL}/api/admin/packages/delete/:id`;
+export const ADD_PACKAGE = `${API_BASE_URL}/admin/packages/add`;
+export const GET_PACKAGE = `${API_BASE_URL}/admin/packages/all`;
+export const UPDATE_PACKAGE = `${API_BASE_URL}/admin/packages/update/:id`; // Backend route: /packages/update/:id
+export const DELETE_PACKAGE = `${API_BASE_URL}/admin/packages/delete/:id`;
 
 
-export const ADD_MEDIA = `${API_BASE_URL}/api/admin/media/add`;
-export const GET_ALL_MEDIA = `${API_BASE_URL}/api/admin/media/all`;
-export const UPDATE_MEDIA = `${API_BASE_URL}/api/admin/media/update`; // :id hum service mein handle karenge
-export const DELETE_MEDIA = `${API_BASE_URL}/api/admin/media/delete`; // :id hum service mein handle karenge
+export const ADD_MEDIA = `${API_BASE_URL}/admin/media/add`;
+export const GET_ALL_MEDIA = `${API_BASE_URL}/admin/media/all`;
+export const UPDATE_MEDIA = `${API_BASE_URL}/admin/media/update`; // :id hum service mein handle karenge
+export const DELETE_MEDIA = `${API_BASE_URL}/admin/media/delete`; // :id hum service mein handle karenge
