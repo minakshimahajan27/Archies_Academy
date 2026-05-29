1fe50f8d03ab5f925fa03540c84a31249f57f594
minakshimahajan27 <minakshi27mahajan2211@>
2026-05-29 21:42:09 +0530

    build pass hogaya final push

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1600,
-    // Yeh line saari faaltu import errors ko ignore karke build pass kar degi
-    rollupOptions: {
-      external: ['../pages/OurTeam', './OurTeam'] 
-    }
-  }
 })
